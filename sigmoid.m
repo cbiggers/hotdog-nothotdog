20ce50d9a0653bfb6550bea4052f0f3df8878429
cbiggers <31355125+cbiggers@users.noreply.github.com>
2018-09-15 19:03:17 -0700

    Removing old Octave files

--- sigmoid.m
+++
@@ -1,7 +0,0 @@
-function g = sigmoid(z)
-## cost function is sigmoid for neural network
-g = 1.0 ./ (1.0 + exp(-z));
-## replace with reLU
-#g = max(0,z);
-
-end
--- sigmoidGradient.m
+++
@@ -1,9 +0,0 @@
-function g = sigmoidGradient(z)
-  
-#### returns the gradient of the sigmoid function #####
-
-g = zeros(size(z));
-
-s = 1 ./ (1 + exp(-z));
-g = s .* (1-s);
-end
